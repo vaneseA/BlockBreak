69d11a0f8d6fba73ae6e565f38136a6cc82eaef4
DK <spol@naver.com>
2022-01-17 21:22:40 +0900

    벽돌꺠기

---
+++ BlockBreak/Functions.swift
@@ -0,0 +1,70 @@
+//
+//  Functions.swift
+//  BlockBreak
+//
+//  Created by DK on 2022/01/17.
+//
+
+import Foundation
+import SpriteKit
+
+extension GameScene {
+    
+    func setting(){
+        let stage = Stages()
+//        tiliting()
+    }
+    
+    
+    //첨 터치 했을 때
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            if Variables.isplayed {
+            Variables.paddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            }else{
+                Variables.paddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+                Variables.ball.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            }
+        }
+    }
+    
+    //터치 후 움직였을 때
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if Variables.isplayed {
+            Variables.paddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            }else{
+                Variables.paddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+                Variables.ball.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            }//드래그
+        }
+    }
+    
+    //터치가 종료되었을 때
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let node : SKNode = self.atPoint(location)
+            if node.name == "paddle"{
+                if !Variables.isplayed {
+                    Variables.isplayed = true
+                    Variables.ball.physicsBody?.isDynamic = true
+                    Variables.ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
+                }
+            }
+        }
+    }
+    
+    //기울기로 패들 움직이기
+    func tiliting(){
+        motion.accelerometerUpdateInterval = 0.1
+        motion.startAccelerometerUpdates(to: .main) { (data, error) in
+            let value = data!.acceleration.y * 1000
+            print("data\(value)")
+            Variables.paddle.run(SKAction.moveTo(x: value, duration: 0.2))
+        }
+    }
+}
---
+++ BlockBreak/Variables.swift
@@ -0,0 +1,24 @@
+//
+//  Variables.swift
+//  BlockBreak
+//
+//  Created by DK on 2022/01/17.
+//
+
+import Foundation
+import SpriteKit
+
+struct Variables {
+    static var scene = SKScene()
+    static var paddle = SKSpriteNode()
+    static var ball = SKShapeNode(circleOfRadius: 10)
+    
+    static let ballCategory : UInt32 = 0x1 << 0     //0000001
+    static let paddleCategory : UInt32 = 0x1 << 1   //000010
+    static let bottomCategory : UInt32 = 0x1 << 2   //000100
+    static let blockCategory : UInt32 = 0x1 << 3    //001000
+    
+    static var blockNum = 0
+    static var isplayed = false
+    
+}
